577950328335370489f43147456f9bb7d7013a00
Alexander Kirk Jørgensen <alkii900@msn.com>
2016-02-18 12:41:48 +0100

    First Commit

---
+++ DetectiveCrime/Assets/ArrestScript.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ArrestScript : MonoBehaviour {
+
+    public GameObject connectedNPC;
+
+    void OnMouseDown()
+    {
+        Debug.Log("You tried to arrest: " + connectedNPC);
+        if (connectedNPC.GetComponent<Murderer>() != null)
+        {
+            Debug.Log("He is the Murderer! Congratz!");
+        } else
+        {
+            Debug.Log("He is not the Murderer! Shit!");
+        }
+    }
+
+    public void SetNPC(GameObject npc)
+    {
+        connectedNPC = npc;
+    }
+}
---
+++ DetectiveCrime/Assets/DummySelector.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Linq;
+using System.Collections.Generic;
+
+public class DummySelector : MonoBehaviour {
+
+    private GameObject[] children;
+
+	// Use this for initialization
+	void Start () {
+        children = (from convObj in GetComponentsInChildren<ConversationObject>().ToList()
+                   select convObj.gameObject).ToArray();
+        var murderer = Random.Range(0, children.Length - 1);
+
+        children[murderer].AddComponent<Murderer>();
+        var murdererDesc = children[murderer].GetComponent<ConversationObject>().descriptors;
+
+        foreach (GameObject go in children)
+        {
+            var newClueIdx = Random.Range(0, murdererDesc.Count() - 1);
+            go.GetComponent<ConversationObject>().SetClue(murdererDesc[newClueIdx]);
+        }
+	}
+}
